4b3c7e2f6864942bdfdf1890e05e470f13e9e42d
wlwc0627 <33441607+actorWen@users.noreply.github.com>
2019-01-10 00:41:15 +0800

    first-initial

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app-container">
+    <myHeader fixed title="vue 商城"></myHeader>
+    <router-view/>
+    <h1 class="content">123</h1>
+    <nav class="mui-bar mui-bar-tab">
+      <a class="mui-tab-item mui-active" href="#tabbar">
+        <span class="mui-icon mui-icon-home"></span>
+        <span class="mui-tab-label">首页</span>
+      </a>
+      <a class="mui-tab-item" href="#tabbar-with-chat">
+        <span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+        <span class="mui-tab-label">消息</span>
+      </a>
+      <a class="mui-tab-item" href="#tabbar-with-contact">
+        <span class="mui-icon mui-icon-contact"></span>
+        <span class="mui-tab-label">通讯录</span>
+      </a>
+      <a class="mui-tab-item" href="#tabbar-with-map">
+        <span class="mui-icon mui-icon-gear"></span>
+        <span class="mui-tab-label">设置</span>
+      </a>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="stylus" scoped>
+  #app-container
+    position: absolute
+    left: 0
+    right: 0
+    top: 0
+    bottom: 0
+    .content
+      margin-top: 40px
+</style>
